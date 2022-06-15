bce9807ed0fd51185ef13d9cea5ac6325dd4c39b
Nemanja Delic <delneman@gmail.com>
2022-06-15 12:36:08 +0000

    adding allowed ciphers

--- site-modules/profile/manifests/puppet/puppetserver.pp
+++ site-modules/profile/manifests/puppet/puppetserver.pp
@@ -4,10 +4,20 @@
 #
 class profile::puppet::puppetserver {
   # Configure puppetdb and its underlying database
+  $allow_ciphers = [
+    'TLS_AES_256_GCM_SHA384',
+    'TLS_AES_128_GCM_SHA256',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256',
+    'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384',
+    'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+  ]
+
   class { 'puppetdb':
     node_ttl       => '0d',
     node_purge_ttl => '1d',
     ssl_protocols  => 'TLSv1.3,TLSv1.2',
+    cipher_suites  => join($allow_ciphers, ','),
   }
   # resources
 }
