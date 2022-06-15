c24e989258897c89848e398f9843432cd805af74
Nemanja Delic <delneman@gmail.com>
2022-06-15 12:22:23 +0000

    node_purge_ttl: '1d'

--- site-modules/profile/manifests/puppet/puppetserver.pp
+++ site-modules/profile/manifests/puppet/puppetserver.pp
@@ -5,7 +5,8 @@
 class profile::puppet::puppetserver {
   # Configure puppetdb and its underlying database
   class { 'puppetdb':
-    node_ttl => '0d', 
+    node_ttl => '0d',
+    node_purge_ttl => '1d',
   }
   # resources
 }
