fba3f242caef3bdf197860457c2a1d76577109d8
Nemanja Delic <delneman@gmail.com>
2022-06-15 12:25:56 +0000

    ssl_protocols: 'TLSv1.3,TLSv1.2'

--- site-modules/profile/manifests/puppet/puppetserver.pp
+++ site-modules/profile/manifests/puppet/puppetserver.pp
@@ -5,8 +5,9 @@
 class profile::puppet::puppetserver {
   # Configure puppetdb and its underlying database
   class { 'puppetdb':
-    node_ttl => '0d',
+    node_ttl       => '0d',
     node_purge_ttl => '1d',
+    ssl_protocols  => 'TLSv1.3,TLSv1.2',
   }
   # resources
 }
