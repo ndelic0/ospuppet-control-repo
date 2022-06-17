268cba8a9aa02944bf34a6ab43b6dcbda9c44f60
Nemanja Delic <delneman@gmail.com>
2022-06-17 06:39:43 +0000

    adding puppetboard

--- site-modules/profile/manifests/puppet/puppetboard.pp
+++ site-modules/profile/manifests/puppet/puppetboard.pp
@@ -1,7 +1,25 @@
 # Manages Puppetboard
 class profile::puppet::puppetboard {
-  class { 'puppetboard':
-    manage_git        => true,
-    manage_virtualenv => true,
+  class {'puppetboard':
+    manage_git          => true,
+    manage_virtualenv   => true,
+    reports_count       => '30',
+    default_environment => '*',
   }
+  class { 'apache':
+    purge_configs => false,
+    mpm_module    => 'prefork',
+    default_vhost => true,
+    default_mods  => false,
+  }
+  class { 'puppetboard::apache::vhost':
+    vhost_name => $facts['fqdn'],
+    port       => 80,
+  }
+  class { 'puppetboard::apache::conf': }
+  class { 'apache::mod::wsgi':
+    wsgi_socket_prefix =>  '/var/run/wsgi',
+  }
+  # Access Puppetboard from example.com/puppetboard
+  class { 'puppetboard::apache::conf': }
 }
