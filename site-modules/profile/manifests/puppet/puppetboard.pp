c685affd364b75f4fbe1917ad1c1a9b81baa9a37
Nemanja Delic <delneman@gmail.com>
2022-06-17 07:35:11 +0000

    fix for puppetboard

--- site-modules/profile/manifests/puppet/puppetboard.pp
+++ site-modules/profile/manifests/puppet/puppetboard.pp
@@ -6,20 +6,7 @@ class profile::puppet::puppetboard {
     reports_count       => 30,
     default_environment => '*',
   }
-  class { 'apache':
-    purge_configs => false,
-    mpm_module    => 'prefork',
-    default_vhost => true,
-    default_mods  => false,
-  }
-  class { 'puppetboard::apache::vhost':
-    vhost_name => $facts['fqdn'],
-    port       => 80,
-  }
-  class { 'puppetboard::apache::conf': }
-  class { 'apache::mod::wsgi':
-    wsgi_socket_prefix =>  '/var/run/wsgi',
-  }
+
   # Access Puppetboard from example.com/puppetboard
   class { 'puppetboard::apache::conf': }
 }
